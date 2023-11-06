231f049ef427eda4c8f10fe385aad363414a7715
rifat3568 <mazharulislam3569@gmail.com>
2023-11-07 04:54:38 +0600

    added mongo user name and pass on dotenv file

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-// const dotenv = require('dotenv');
-// dotenv.config();
+require('dotenv').config()
 const port = process.env.PORT || 5001;
 
 // middlewar
@@ -11,9 +10,7 @@ app.use(cors())
 app.use(express.json());
 
 
-// 8R87qornb9k38O0L
-
-const uri = "mongodb+srv://mazharulislam3569:8R87qornb9k38O0L@cluster0.blnxmvz.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.blnxmvz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,11 +27,11 @@ async function run() {
     await client.connect();
 
     const jobsCollection = client.db('JobsDB').collection('allJobs')
+    const applyCollection = client.db('JobsDB').collection('applyFrom')
 
 
 
     app.post('/addJobs',async (req,res)=>{
-        console.log('post hitting');
         const jobs = req.body
         console.log(jobs);
         const result = await jobsCollection.insertOne(jobs)
@@ -42,6 +39,19 @@ async function run() {
         res.send(result)
     })
 
+    app.post('/apply',async (req,res)=>{
+        const apply = req.body
+        console.log(apply);
+        const result = await applyCollection.insertOne(apply)
+        console.log(result);
+        res.send(result)
+    })
+
+    app.get("/apply", async (req, res) => {
+        const result = await applyCollection.find().toArray();
+        res.send(result);
+      });
+      
     app.get("/addJobs", async (req, res) => {
         const result = await jobsCollection.find().toArray();
         res.send(result);
